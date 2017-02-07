b8427ed2b4bf7001e3cd22d0d4d22f24bac449f6
Plastikaweb <info@plastikaweb.com>
2017-02-07 16:29:40 +0100

    extract get getLocationData from ngInit to a button click event

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,16 +28,16 @@ describe('AppComponent: geofire simple exemple', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'geofire app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('geofire app');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('geofire app');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AngularFire} from 'angularfire2';
+import {AngularFire, AngularFireDatabase} from 'angularfire2';
 import * as Geofire from 'geofire';
 
 @Component({
@@ -8,77 +8,66 @@ import * as Geofire from 'geofire';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
-  products = [];
+  title = 'geofire app';
+  locations = [];
+  afRef: AngularFireDatabase;
+  gfRef: any;
 
-
-  constructor(private af: AngularFire) {
-  }
+  constructor(private af: AngularFire) {}
 
   ngOnInit() {
+    this.afRef = this.af.database;
     const gf: any = Geofire;
-    const afRef = this.af.database;
-    const locationsList = afRef.list('/locations');
-    const gfRef = new gf(locationsList.$ref);
+    const locationsList = this.afRef.list('/locations');
+    this.gfRef = new gf(locationsList.$ref);
+
     locationsList
-      .subscribe(data => this.products = data);
+      .subscribe(data => this.locations = data);
+  }
 
-    // const fbRef = database().ref().push();
-    // const geoFire = new GeoFire(fbRef);
-
-    /* Callback method from the geolocation API which receives the current user's location */
-    const geolocationCallback = (location) => {
-      const latitude = location.coords.latitude;
-      const longitude = location.coords.longitude;
-      console.log('Retrieved user location: [' + latitude + ', ' + longitude + ']');
-
-      const place = {
-        name: 'place 1',
-        description: 'description for place 1',
-        pic: 'place1.jpg'
-      };
-      const places = afRef.list('/places');
-      places.push(place)
-        .then((data) => {
-          const key = data.key;
-          gfRef.set(key, [latitude, longitude]).then(function () {
-            console.log('Current place ' + key + ' location has been added to GeoFire');
-
-            // When the user disconnects from Firebase (e.g. closes the app, exits the browser),
-            // remove their GeoFire entry
-            gfRef.child(key).onDisconnect().remove();
-
-            console.log('Added handler to remove place ' + key + ' from GeoFire when you leave this page.');
-          }).catch(function (error) {
-            console.log('Error adding place ' + key + 's location to GeoFire');
-          });
+  /* Callback method from the geolocation API which receives the current user's location */
+  geolocationCallback = (location) => {
+    const latitude = location.coords.latitude;
+    const longitude = location.coords.longitude;
+    console.log('Retrieved user location: [' + latitude + ', ' + longitude + ']');
+    const gfRef = this.gfRef;
+    const place = {
+      name: 'place 1',
+      description: 'description for place 1',
+      pic: 'place1.jpg'
+    };
+    const places = this.afRef.list('/places');
+    places.push(place)
+      .then((data) => {
+        const key = data.key;
+        gfRef.set(key, [latitude, longitude]).then(function () {
+          console.log('Current place ' + key + ' location has been added to GeoFire');
+        }).catch(function (error) {
+          console.error(error);
+          console.log('Error adding place ' + key + ' location to GeoFire');
         });
-    };
+      });
+  };
 
-    /* Handles any errors from trying to get the user's current location */
-    const errorHandler = (error) => {
-      if (error.code === 1) {
-        console.log('Error: PERMISSION_DENIED: User denied access to their location');
-      } else if (error.code === 2) {
-        console.log('Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached');
-      } else if (error.code === 3) {
-        console.log('Error: TIMEOUT: Calculating the user location too took long');
-      } else {
-        console.log('Unexpected error code');
-      }
-    };
+  /* Handles any errors from trying to get the user's current location */
+  errorHandler = (error) => {
+    if (error.code === 1) {
+      console.log('Error: PERMISSION_DENIED: User denied access to their location');
+    } else if (error.code === 2) {
+      console.log('Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached');
+    } else if (error.code === 3) {
+      console.log('Error: TIMEOUT: Calculating the user location too took long');
+    } else {
+      console.log('Unexpected error code');
+    }
+  };
 
-    /* Uses the HTML5 geolocation API to get the current user's location */
-    const getLocation = () => {
-      if (typeof navigator !== 'undefined' && typeof navigator.geolocation !== 'undefined') {
-        console.log('Asking user to get their location');
-        navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
-      } else {
-        console.log('Your browser does not support the HTML5 Geolocation API, so this demo will not work.');
-      }
-    };
-
-    getLocation();
-
+  getLocationData = () => {
+    if (typeof navigator !== 'undefined' && typeof navigator.geolocation !== 'undefined') {
+      console.log('Asking user to get their location');
+      navigator.geolocation.getCurrentPosition(this.geolocationCallback, this.errorHandler);
+    } else {
+      console.log('Your browser does not support the HTML5 Geolocation API, so this demo will not work.');
+    }
   }
 }
